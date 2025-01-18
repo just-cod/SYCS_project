52d424801a8c88fbe3080452ea70ca10ec3a6cd8
just-cod <143872028+just-cod@users.noreply.github.com>
2025-01-18 14:55:57 +0000

    18 jan 2025

--- refrence/scrip.js
+++ refrence/scrip.js
@@ -1,17 +1,31 @@
 const form = document.getElementsByTagName(`form`);
 
-const easy = document.querySelector(`easy`);
-const medium = document.querySelector(`medium`);
-const hard = document.querySelector(`hard`);
+const easy = document.querySelector(`.easy`);
+const medium = document.querySelector(`.medium`);
+const hard = document.querySelector(`.hard`);
 
-easy.addEventListner(`click`, () => {
-  alert(`easy`);
-});
+// easy.addEventListner(`click`, () => {
+//   alert(`easy`);
+// });
 
-medium.addEventListener(`click`, () => {
-  document.write(`medium`);
-});
+// medium.addEventListener(`click`, () => {
+//   document.write(`medium`);
+// });
 
-hard.addEventListener(`click`, () => {
-  document.write(`hard`);
+// hard.addEventListener(`click`, () => {
+//   document.write(`hard`);
+// });
+
+let difficulty = 0;
+
+function  setDifficulty(level) {
+  difficulty = level;
+  alert(difficulty);
+}
+
+// document.querySelector(`.easy`).addEventListener(`click`, setDifficulty(20));
+
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.querySelector('.container');
+  container.innerHTML = '<p>This is a dynamically added paragraph.</p>';
 });
