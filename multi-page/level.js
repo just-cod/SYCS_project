5e3f5f9d7f074768bb29479a5a992ffdc060dd78
just-cod <143872028+just-cod@users.noreply.github.com>
2025-01-25 16:34:33 +0000

    25 jan2025

---
+++ multi-page/level.js
@@ -0,0 +1,9 @@
+`use strict`;
+
+// home btn
+export const back = document.querySelector(`.back`);
+
+// text
+export const text = document.querySelector(`.text`);
+
+export const level = document.querySelectorAll(`.level`)
